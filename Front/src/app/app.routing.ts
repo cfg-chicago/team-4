22a8ba6fd75416ea806a267de8e1df80b021914f
Kenneth Ho <kennyho1516@gmail.com>
2017-09-29 20:32:24 -0500

    Add Angular CLI

---
+++ Front/src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+export const ROUTES: Routes = [
+    {
+        path: 'home', 
+        component: HomeComponent
+    },
+    
+    {
+    	path: '',
+    	redirectTo: '/home',
+    	pathMatch: 'full'
+  	},
+  	{ 
+  		path: '**',
+  		component: HomeComponent
+  	}
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(ROUTES) ],
+  exports: [ RouterModule ],
+  providers: []
+})
+export class Router { }
